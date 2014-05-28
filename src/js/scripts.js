302edaf72cf64b620acd65b5269a7f98d815d645
Ericson Luciano <design.ericson@gmail.com>
2014-05-29 05:16:26 +0800

    add homepage and other page uncomplete

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,19 @@
-// (function ($, window, document, undefined) {
+(function ($, window, document, undefined) {
 
-//   'use strict';
+  'use strict';
+	
+  $(window).scroll(function() {
+    if ($('.navbar').offset().top > 50) {
+      $('.navbar-fixed-top').addClass('top-nav-collapse');
+      $('.navbar').addClass('navbar-scroll');
+      $('.logo').addClass('logo-resize');
+      $('.logo').attr('src', 'assets/img/logo-small.svg');
+    } else {
+      $('.navbar-fixed-top').removeClass('top-nav-collapse');
+      $('.navbar').removeClass('navbar-scroll');
+      $('.logo').removeClass('logo-resize');
+      $('.logo').attr('src', 'assets/img/logo.svg');
+    }
+  });
 
-//   $(function () {
-//     // FireShell
-//   });
-
-
-
-// })(jQuery, window, document);
+})(jQuery, window, document);
